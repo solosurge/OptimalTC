89fccc3238c281e546f7642936963122eb3ad65b
solosurge <solosurge@yahoo.com>
2021-02-05 18:25:25 -0600

    End of section 8

---
+++ API/DTOs/MemberDto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace API.DTOs
+{
+    public class MemberDto
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+        public DateTime Created { get; set; } 
+
+        public DateTime LastActive { get; set; }
+
+        public string Information { get; set; }
+
+        public string City { get; set; }
+        
+        public ICollection<PhotoDto> Photos { get; set; }
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,10 +1,25 @@
+using System;
+using System.Collections.Generic;
+using API.Extensions;
+
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-        public byte[] PasswordHash {get; set;}
-        public byte[] PasswordSalt {get; set;}        
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt  { get; set; }
+        //public DateTime DateOfBirth { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Information { get; set; }
+        public string City { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
+        /* public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        } */
     }
 }
